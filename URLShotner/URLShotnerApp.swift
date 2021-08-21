24c588a71f01fff2daa60e612e77eec4b4a5cffd
Sergei Matveev <sgsq@ya.ru>
2021-08-21 10:37:46 +0400

    Initial Commit

---
+++ URLShotner/URLShotnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLShotnerApp.swift
+//  URLShotner
+//
+//  Created by Sergei on 21.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct URLShotnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
